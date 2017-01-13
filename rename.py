6c6d8dbafd5a5516ef3f78dd5b32f20565a51bdf
WPB <bill@briskynet.com>
2017-01-12 18:29:45 -0600

    First changes.  Printed asset id and creation date

--- rename.py
+++ rename.py
@@ -1,3 +1,5 @@
+#! python3
+
 import photos
 
 all_assets = photos.get_assets()
@@ -8,11 +10,17 @@ smart_albums = photos.get_smart_albums()
 
 other_albums = photos.get_albums()
 
-print ("Smart albums count: {}".format(len(smart_albums)))
+print ("\nSmart albums count: {}".format(len(smart_albums)))
 for album in smart_albums:
 	print("\tTitle: {}. Type:{} Count:{}".format(album.title, album.type, len(album.assets)))
+	for asset in album.assets:
+		print("\t\tid:{}\tdate:{}".format(asset.id, asset.creation_date))
+
 print ("\nOther albums count: {}".format(len(other_albums)))
 for album in other_albums:
 	print("\tTitle: {}. Type:{} Count:{}".format(album.title, album.type, len(album.assets)))
+	for asset in album.assets:
+		print("\t\tid:{}\tdate:{}".format(asset.id, asset.creation_date))
+
 a = 25
 
